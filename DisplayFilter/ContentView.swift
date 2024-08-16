42df6b4bb7ce430e5d4fdb8bd9b8f1489a3a43d5
Yaşar <8968758+uyasarkocal@users.noreply.github.com>
2024-08-16 08:56:07 +0300

    Initial Commit

---
+++ DisplayFilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DisplayFilter
+//
+//  Created by Yasar Kocal on 16.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DisplayFilter/DisplayFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayFilterApp.swift
+//  DisplayFilter
+//
+//  Created by Yasar Kocal on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DisplayFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
